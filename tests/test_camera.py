0c0fede657a09286112671f9f0e33c55751dc6b9
Paul Kilgo <pkilgo@clemson.edu>
2015-06-26 15:41:37 -0400

    unit tests

---
+++ tests/test_camera.py
@@ -0,0 +1,70 @@
+from volpy import Camera
+
+import numpy as np
+import numpy.testing as npt
+
+import unittest
+
+x = np.array([1, 0, 0])
+y = np.array([0, 1, 0])
+z = np.array([0, 0, 1])
+
+
+class CameraTest(unittest.TestCase):
+
+    def setUp(self):
+        self.camera = Camera([0, 0, 0], [0, 0, 1])
+
+    def test_near1(self):
+        self.assertEqual(0.1, self.camera.near)
+
+    def test_far1(self):
+        self.assertEqual(2., self.camera.far)
+
+    def test_eye1(self):
+        self.assertIsInstance(self.camera.eye, np.ndarray)
+        self.assertEqual(self.camera.eye.dtype, np.dtype(np.float32))
+        npt.assert_almost_equal([0, 0, 0], self.camera.eye)
+
+    def test_eye2(self):
+        self.camera.eye = (0, 1, 1)
+        self.assertIsInstance(self.camera.eye, np.ndarray)
+        npt.assert_almost_equal([0, 1, 1], self.camera.eye)
+
+    def test_view1(self):
+        self.assertIsInstance(self.camera.view, np.ndarray)
+        npt.assert_almost_equal([0, 0, 1], self.camera.view)
+        npt.assert_almost_equal(0, self.camera.up.dot(self.camera.view))
+
+    def test_view2(self):
+        self.camera.view = [2, 0, 0]
+        self.assertIsInstance(self.camera.view, np.ndarray)
+        npt.assert_almost_equal([1, 0, 0], self.camera.view)
+        npt.assert_almost_equal(0, self.camera.up.dot(self.camera.view))
+
+    def test_up1(self):
+        self.assertIsInstance(self.camera.up, np.ndarray)
+        npt.assert_almost_equal([0, 1, 0], self.camera.up)
+        npt.assert_almost_equal(0, self.camera.up.dot(self.camera.view))
+
+    def test_up2(self):
+        self.camera.up = [2, 0, 0]
+        self.assertIsInstance(self.camera.up, np.ndarray)
+        npt.assert_almost_equal([1, 0, 0], self.camera.up)
+        npt.assert_almost_equal(0, self.camera.up.dot(self.camera.view))
+
+    def test_right1(self):
+        self.assertIsInstance(self.camera.right, np.ndarray)
+        npt.assert_almost_equal(0, self.camera.up.dot(self.camera.right))
+        npt.assert_almost_equal(0, self.camera.view.dot(self.camera.right))
+
+    def test_right2(self):
+        with self.assertRaises(AttributeError):
+            self.camera.right = (0, 1, 0)
+
+    def test_cast1(self):
+        origin, direction = self.camera.cast([0.5], [0.5])
+        self.assertIsInstance(origin, np.ndarray)
+        self.assertIsInstance(direction, np.ndarray)
+        npt.assert_almost_equal(origin, [[0, 0, 0.1]])
+        npt.assert_almost_equal(direction, [[0, 0, 1]])
