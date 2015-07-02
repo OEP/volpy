015f472ce8d610f4b9cba6aa48607801d263c339
Paul Kilgo <pkilgo@clemson.edu>
2015-07-02 14:28:28 -0400

    test grid default value parameter

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -42,6 +42,36 @@ class ScalarGridTestCase(unittest.TestCase):
         npt.assert_almost_equal(expected, result)
 
 
+class DefaultValueTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.grid = volpy.Grid(np.ones((100, 100, 100)), default=-1)
+
+    def test_call1(self):
+        '''Out of bounds returns default value'''
+        result = self.grid([[-0.1, -0.1, -0.1]])
+        expected = [-1]
+        npt.assert_almost_equal(expected, result)
+
+    def test_call2(self):
+        '''In bounds returns grid value'''
+        result = self.grid([[0.5, 0.5, 0.5]])
+        expected = [1]
+        npt.assert_almost_equal(expected, result)
+
+    def test_call3(self):
+        '''One out of bounds coordinate returns default value (positive)'''
+        result = self.grid([[0.5, 100, 0.5]])
+        expected = [-1]
+        npt.assert_almost_equal(expected, result)
+
+    def test_call4(self):
+        '''One out of bounds coordinate returns default value (negative)'''
+        result = self.grid([[-0.1, 0.5, 0.5]])
+        expected = [-1]
+        npt.assert_almost_equal(expected, result)
+
+
 class VectorGridTestCase(unittest.TestCase):
 
     def setUp(self):
