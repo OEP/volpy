58ea8d5bd8bff78574067a7a6bf82b383a465570
Paul Kilgo <paulkilgo@gmail.com>
2015-07-08 10:40:00 -0400

    docstrings for volpy.homogeneous

--- volpy/homogenous.py
+++ volpy/homogenous.py
@@ -1,9 +1,31 @@
+'''
+Functions for homogeneous coordinate systems.
+
+'''
+
 import numpy as np
 
 import math
 
 
 def translate(vx, vy, vz):
+    '''
+    Create a translation transformation matrix.
+
+    Parameters
+    ----------
+    vx : scalar
+        Translation amount in the X direction.
+    vy : scalar
+        Translation amount in the Y direction.
+    vz : scalar
+        Translation amount in the Z direction.
+
+    Returns
+    -------
+    T : array
+        A 4x4 matrix for homogeneous coordinate translation.
+    '''
     return np.array([[1, 0, 0, vx],
                      [0, 1, 0, vy],
                      [0, 0, 1, vz],
@@ -11,6 +33,23 @@ def translate(vx, vy, vz):
 
 
 def scale(sx, sy, sz):
+    '''
+    Create a scaling transformation matrix.
+
+    Parameters
+    ----------
+    sx : scalar
+        Scaling amount in the X direction.
+    sy : scalar
+        Scaling amount in the Y direction.
+    sz : scalar
+        Scaling amount in the Z direction.
+
+    Returns
+    -------
+    S : array
+        A 4x4 matrix for homogeneous coordinate scaling.
+    '''
     return np.array([[sx, 0, 0, 0],
                      [0, sy, 0, 0],
                      [0, 0, sz, 0],
@@ -18,6 +57,22 @@ def scale(sx, sy, sz):
 
 
 def rotate_axis(axis, theta):
+    '''
+    Rotation matrix for arbitrary axis-angle rotations.
+
+    Parameters
+    ----------
+    axis : array-like
+        A length 3 array representing the axis to rotate across. This should be
+        normalized by the caller.
+    theta : scalar
+        Angle in radians.
+
+    Returns
+    -------
+    R : array
+        A 4x4 matrix for homogeneous coordinate rotations.
+    '''
     axis = np.asarray(axis)
     a = math.cos(theta / 2)
     b, c, d = -axis * math.sin(theta / 2)
@@ -45,23 +100,99 @@ def rotate_axis(axis, theta):
 
 
 def rotatex(theta):
+    '''
+    Rotation matrix for rotating across the X axis. This is a convenience for
+    ``rotate_axis``.
+
+    Parameters
+    ----------
+    theta : scalar
+        Angle in radians.
+
+    Returns
+    -------
+    R : array
+        A 4x4 matrix for homogeneous coordinate rotations.
+    '''
     return rotate_axis([1, 0, 0], theta)
 
 
 def rotatey(theta):
+    '''
+    Rotation matrix for rotating across the Y axis. This is a convenience for
+    ``rotate_axis``.
+
+    Parameters
+    ----------
+    theta : scalar
+        Angle in radians.
+
+    Returns
+    -------
+    R : array
+        A 4x4 matrix for homogeneous coordinate rotations.
+    '''
     return rotate_axis([0, 1, 0], theta)
 
 
 def rotatez(theta):
+    '''
+    Rotation matrix for rotating across the Z axis. This is a convenience for
+    ``rotate_axis``.
+
+    Parameters
+    ----------
+    theta : scalar
+        Angle in radians.
+
+    Returns
+    -------
+    R : array
+        A 4x4 matrix for homogeneous coordinate rotations.
+    '''
     return rotate_axis([0, 0, 1], theta)
 
 
 def rotatexyz(alpha, beta, gamma):
+    '''
+    Rotation matrix for rotating across the X, Y, and Z axes, in that order.
+    This is a convenience for ``rotate_axis``.
+
+    Parameters
+    ----------
+    alpha : scalar
+        Angle in radians to rotate across the X axis.
+    beta : scalar
+        Angle in radians to rotate across the Y axis.
+    gamma : scalar
+        Angle in radians to rotate across the Z axis.
+
+    Returns
+    -------
+    R : array
+        A 4x4 matrix for homogeneous coordinate rotations.
+    '''
     return rotatez(alpha).dot(rotatey(beta).dot(rotatex(gamma)))
 
 
 def cross(u, v, dtype=float):
-    '''Cross product for homogenous vectors'''
+    '''
+    Cross product for homogeneous vectors.
+
+    Parameters
+    ----------
+    u : array-like
+        First homogeneous vector.
+    v : array-like
+        Second homogeneous vector.
+    dtype : data-type, optional
+        A numpy data type.
+
+    Returns
+    -------
+    R : array
+        A 4x4 matrix for homogeneous coordinate rotations.
+    '''
     u = np.asarray(u)
     v = np.asarray(v)
     if not u.shape == (4,) or not v.shape == (4,):
