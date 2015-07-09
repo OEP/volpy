3cac03f6ce3e61937f492a6e26657a2dc984226e
Paul Kilgo <pkilgo@clemson.edu>
2015-07-09 16:46:50 -0400

    documentation for grid

--- volpy/grid.py
+++ volpy/grid.py
@@ -1,3 +1,7 @@
+'''
+Fast grid evaluation for NumPy arrays.
+
+'''
 import numpy as np
 
 from ._grid import grid_scalar_eval, grid_vector_eval
@@ -7,8 +11,28 @@ MAX_COORDINATE = np.array([0.5, 0.5, 0.5, 1])
 
 
 class Grid(object):
+    '''
+    A wrapper object for convenient evaluation and interpolation of NumPy
+    arrays. The evaluation happens inside of a C extension so it is much
+    faster, and releases the global interpreter lock so multiple threads can
+    execute in the meantime.
+    '''
 
     def __init__(self, array, transform=None, default=0):
+        '''
+        Grid constructor.
+
+        Parameters
+        ----------
+        array : array-like
+            Any scalar or vector array.
+        transform : array-like
+            A 4x4 transformation matrix which maps world coordinates to the
+            default normalized grid coordinates.
+        default : float
+            A default value for points outside of the grid. This value will be
+            broadcasted into the result for vector arrays.
+        '''
         self.array = np.asarray(array, dtype=np.float64)
         self.default = default
         if transform is None:
