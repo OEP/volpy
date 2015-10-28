fe32099bf1b6aa387c98dd6afdfc31557fc4e1f9
Paul Kilgo <paulkilgo@gmail.com>
2015-10-27 20:17:18 -0400

    Write a docstring for the package

--- volpy/__init__.py
+++ volpy/__init__.py
@@ -1,3 +1,24 @@
+'''
+Volpy
+=====
+
+A fast volume rendering implementation for Python. Volpy has support for:
+
+  1. Multithreading or multiprocessing at the rendering step
+  2. Native implementation of ray casting
+  3. Native access to NumPy arrays during rendering
+  4. Support for ambient and diffuse lighting terms
+
+How to use this package
+-----------------------
+
+Volpy is organized into several different modules but the API is imported into
+the root of the package. Therefore, you should write your code like this:
+
+    >>> import volpy
+    >>> scene = volpy.Scene(ambient=my_func)
+
+'''
 from .camera import Camera
 from .scene import Scene, Element, Light
 from .version import __version__
