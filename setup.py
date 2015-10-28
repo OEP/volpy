38968b0c422c46e90e869d506b8224102420222f
Paul Kilgo <pkilgo@clemson.edu>
2015-10-28 13:25:57 -0400

    Disable boundscheck by default

--- setup.py
+++ setup.py
@@ -8,11 +8,15 @@ os.chdir(os.path.normpath(os.path.dirname(__file__)))
 __version__ = None
 exec(open('volpy/version.py').read())
 
+compiler_directives = {
+    'boundscheck': False,
+}
 
 setup(
     name='volpy',
     version=__version__,
-    ext_modules=cythonize('volpy/*.pyx'),
+    ext_modules=cythonize('volpy/*.pyx',
+                          compiler_directives=compiler_directives),
     include_dirs=[np.get_include()],
     packages=['volpy'],
     include_package_data=True,
